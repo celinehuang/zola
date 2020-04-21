509acbe83ba3ade6c2347b82bfd352686c518de8
celinehuang <celine756@hotmail.com>
2020-04-21 16:35:49 -0400

    Add sort by functionality

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,32 +1,46 @@
 <template>
   <div>
-    <q-select
+    <q-input
       rounded
       outlined
-      v-model="model"
-      :options="options"
+      v-model="text"
       label="Search"
       maxlength="12"
-      class="q-ma-md"
+      class="q-pa-lg q-mx-sm q-mt-sm"
     >
-      <!-- <template v-slot:before>
-        <q-icon name="search" />
-      </template> -->
-
       <template v-slot:append>
-        <q-icon
-          v-if="model !== ''"
-          name="close"
-          @click.stop="model = ''"
-          class="cursor-pointer"
-        />
-        <q-icon name="search" @click.stop />
+        <q-icon name="search" />
       </template>
+    </q-input>
 
-      <template v-slot:hint>
-        Field hint
-      </template>
-    </q-select>
+    <q-btn-dropdown flat class="float-right q-ma-md" label="Sort By">
+      <q-list>
+        <q-item clickable v-close-popup @click="onLowestPriceClick">
+          <q-item-section>
+            <q-item-label>Lowest Price</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onHighestPriceClick">
+          <q-item-section>
+            <q-item-label>Highest Price</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onLatestClick">
+          <q-item-section>
+            <q-item-label>Latest Release Date</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onOldestClick">
+          <q-item-section>
+            <q-item-label>Oldest Release Date</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+
     <div class="container">
       <div class="q-pa-md row justify-center items-start q-gutter-md">
         <Item
@@ -51,12 +65,70 @@ import Item from "../components/Item.vue";
 export default {
   data() {
     return {
-      items: null
+      items: null,
+      text: null
     };
   },
-  methods: {},
   components: {
     Item
+  },
+  methods: {
+    onLowestPriceClick() {
+      this.items.sort(function(x, y) {
+        if (x.price < y.price) {
+          return -1;
+        }
+        if (x.price > y.price) {
+          return 1;
+        }
+        return 0;
+      });
+      this.items.forEach(item => {
+        console.log(item.price);
+      });
+    },
+    onHighestPriceClick() {
+      this.items.sort(function(x, y) {
+        if (x.price < y.price) {
+          return 1;
+        }
+        if (x.price > y.price) {
+          return -1;
+        }
+        return 0;
+      });
+      this.items.forEach(item => {
+        console.log(item.price);
+      });
+    },
+    onLatestClick() {
+      this.items.sort(function(x, y) {
+        if (x.release_year < y.release_year) {
+          return 1;
+        }
+        if (x.release_year > y.release_year) {
+          return -1;
+        }
+        return 0;
+      });
+      this.items.forEach(item => {
+        console.log(item.release_year);
+      });
+    },
+    onOldestClick() {
+      this.items.sort(function(x, y) {
+        if (x.release_year < y.release_year) {
+          return -1;
+        }
+        if (x.release_year > y.release_year) {
+          return 1;
+        }
+        return 0;
+      });
+      this.items.forEach(item => {
+        console.log(item.release_year);
+      });
+    }
   },
   created() {
     this.$axios
