3e9fbb2c50dd15f608cdbbb937e9dc1ae6f8a9f8
celinehuang <celine756@hotmail.com>
2020-04-21 12:26:15 -0400

    Change backend folder name

--- backend/Zola/vueapi/urls.py
+++ backend/Zola/vueapi/urls.py
@@ -1,28 +1,20 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from .views import (
-    ProfileViewSet,
-    ItemViewSet,
-    PaymentViewSet,
-    ProfilePartialUpdateView,
-    MessageViewSet,
-    ItemSearchView,
-)
+from .views import ProfileViewSet, ItemViewSet, PaymentViewSet, ProfilePartialUpdateView, MessageViewSet
 
 router = routers.DefaultRouter()
-# router.register('profiles', ProfileViewSet)
-router.register("items", ItemViewSet)
-router.register("payments", PaymentViewSet)
-router.register("messages", MessageViewSet)
+#router.register('profiles', ProfileViewSet)
+router.register('items', ItemViewSet)
+router.register('payments',PaymentViewSet)
+router.register('messages', MessageViewSet)
 
 from . import views
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("itemsearch/", ItemSearchView.as_view()),
-    path("users/", ProfileViewSet.as_view()),
-    path("rest-auth/", include("rest_auth.urls")),
-    path("rest-auth/registration/", include("rest_auth.registration.urls")),
-    path("partialupdate/<int:pk>/", ProfilePartialUpdateView.as_view()),
+    path('', include(router.urls)),
+    path('users/', ProfileViewSet.as_view()),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('partialupdate/<int:pk>/', ProfilePartialUpdateView.as_view())
 ]
--- backend/Zola/vueapi/views.py
+++ backend/Zola/vueapi/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import viewsets, status, generics, mixins, filters
-from .serializers import (
-    ProfileSerializer,
-    ItemSerializer,
-    PaymentSerializer,
-    UserSerializer,
-    MessageSerializer,
-)
+from rest_framework import viewsets, status, generics, mixins
+from .serializers import ProfileSerializer, ItemSerializer, PaymentSerializer, UserSerializer, MessageSerializer
 from .models import Profile, Item, Payment, Message
 from rest_framework.response import Response
-from rest_framework.decorators import (
-    permission_classes,
-    api_view,
-    action,
-    authentication_classes,
-)
+from rest_framework.decorators import permission_classes, api_view, action, authentication_classes
 from rest_framework import permissions
 from django.views.generic.base import TemplateResponseMixin
 
@@ -24,46 +13,51 @@ class ProfileViewSet(generics.ListAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-
-class ProfilePartialUpdateView(
-    generics.GenericAPIView, mixins.UpdateModelMixin, TemplateResponseMixin
-):
-    """
+class ProfilePartialUpdateView(generics.GenericAPIView, mixins.UpdateModelMixin, TemplateResponseMixin):
+    '''
     You just need to provide the field which is to be modified.
-    """
-
+    '''
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    content_type = "multipart/form-data"
+    content_type='multipart/form-data'
+
 
     def patch(self, request, *args, **kwargs):
-        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
         return self.partial_update(request, *args, **kwargs)
 
-
-class ItemViewSet(
-    viewsets.ModelViewSet, mixins.UpdateModelMixin, TemplateResponseMixin
-):
+class ItemViewSet(viewsets.ModelViewSet,  mixins.UpdateModelMixin, TemplateResponseMixin):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    content_type = "multipart/form-data"
+    content_type = 'multipart/form-data'
 
     def put(self, request, *args, **kwargs):
-        print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+        print('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
         return self.partial_update(request, *args, **kwargs)
+    # @action(methods=['POST'], detail=True)
+    # def change_description(self, request, pk=None):
+    #     if 'description' in request.data:
+    #         item = Item.objects.get(id=pk)
+    #         newdesc = request.data['description']
+    #         try:    
+    #             item.description = newdesc
+    #             item.save()
+    #             serializer = ItemSerializer(item, many=False)
+    #             response={'message':'desc has been updated', 'result':serializer.data}
+    #             return Response(response, status=status.HTTP_200_OK)
 
-
-class ItemSearchView(generics.ListCreateAPIView):
-    search_fields = ["artist", "title", "description"]
-    filter_backends = (filters.SearchFilter,)
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
-
+    #         except:
+    #             console.log('an error occured.')
+    #             response={'message':'error occured'}
+    #             return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    #     else:
+    #         console.log('an error occured.')
+    #         response={'message':'error occured'}
+    #         return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
